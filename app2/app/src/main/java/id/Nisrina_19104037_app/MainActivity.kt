c8f2433b9e05f0e725d4bfe7018f01d8ac57f27c
Nisrinaeka <72739696+Nisrinaeka@users.noreply.github.com>
2021-11-14 22:23:37 -0800

    Add files via upload

---
+++ app2/app/src/main/java/id/Nisrina_19104037_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.Nisrina_19104037_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
