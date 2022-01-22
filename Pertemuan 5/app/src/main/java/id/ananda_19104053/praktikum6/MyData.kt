9f714bb213dad887464a2cf31375ca827d81cbed
Nisrinaeka <72739696+Nisrinaeka@users.noreply.github.com>
2022-01-22 13:49:07 +0700

    Add files via upload

---
+++ Pertemuan 5/app/src/main/java/id/ananda_19104053/praktikum6/MyData.kt
@@ -0,0 +1,7 @@
+package id.ananda_19104053.praktikum6
+
+data class MyData (
+    var name: String,
+    var description: String,
+    var photo: String
+)
---
+++ Pertemuan 5/app/src/main/java/id/ananda_19104053/praktikum6/adapter/ListMyDataAdapter.kt
@@ -0,0 +1,15 @@
+package id.ananda_19104053.praktikum6.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import id.ananda_19104053.praktikum6.MyData
+
+class ListMyDataAdapter(private val listMyData:Array<MyData>):
+RecyclerView.Adapter<ListMyDataAdapter.ListViewHolder>(){
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ListViewHolder {
+        v
+    }
+}
